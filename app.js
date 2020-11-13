6b69ae1dbb391fb745b02f629f299ad4ebd185e4
Eyoab Tesfaye <eyoabtesfaye10@gmail.com>
2020-11-13 12:59:56 +0300

    added database connection

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 // load environment varibles from .env file into process.env
 dotenv.config({path: './config/config.env'})
 
+// load DB connection
+connectDB()
+
+// initialize app
 const app = express()
 
 const PORT = process.env.PORT || 5000
